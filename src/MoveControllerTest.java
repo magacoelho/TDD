0ab2bad7d23406b045a7bd684b63a172fd0fb069
bl0819 <bl0819@192.168.7.1>
2015-03-26 13:50:52 +0100

    ADD  class CARD

--- src/MoveControllerTest.java
+++ src/MoveControllerTest.java
@@ -8,15 +8,16 @@ public class MoveControllerTest {
    private MoveController moveController;
 	 @Before
 	 public void before(){
-		 
 		 moveController= new MoveController();
-		 
 	 }
-	
-	
 	@Test
 	public void moveFromDeckToWasteTest() {
-		fail("Not yet implemented");
+		 Card card = new Card(0, 3);
+		 Waste waste = new Waste();
+		 Deck deck = new Deck();
+	     assertTrue(moveController.moveFromDeckToWaste(card,deck, waste));
+		
+		
 	}
 
 }
