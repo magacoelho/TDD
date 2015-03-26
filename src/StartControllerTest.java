34394234e4c6c0720d90962daa91fd7703ab0b13
bl0819 <bl0819@192.168.7.1>
2015-03-26 13:29:54 +0100

    INITIAL

---
+++ src/StartControllerTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class StartControllerTest {
+    private  StartController startController;
+	@Before
+	public void before(){
+		
+		startController = new StartController();
+		
+	}
+    
+    @Test
+	public void test() {
+		
+		
+    	fail("Not yet implemented");
+	}
+
+}
