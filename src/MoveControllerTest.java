09c060d7d843ae43b8eb377c18b6e8b31bb09c9a
bl0819 <bl0819@192.168.7.1>
2015-03-26 13:36:27 +0100

    ADD moveFromDeckToWasteTest()

--- src/MoveControllerTest.java
+++ src/MoveControllerTest.java
@@ -1,15 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class MoveControllerTest {
    private MoveController moveController;
-	
+	 @Before
+	 public void before(){
+		 
+		 moveController= new MoveController();
+		 
+	 }
 	
 	
 	@Test
-	public void test() {
+	public void moveFromDeckToWasteTest() {
 		fail("Not yet implemented");
 	}
 
