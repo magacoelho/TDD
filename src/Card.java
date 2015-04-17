144ad09ea2be4597734f599b28957f85b33629f3
bl0819 <bl0819@192.168.7.1>
2015-04-17 19:55:54 +0200

    UPADTE Card...constructor getters setters

--- src/Card.java
+++ src/Card.java
@@ -1,12 +1,35 @@
 
 public class Card {
-    private Color color;
+    private Colour colour;
     private int number;
     private boolean covered;
-	public Card(int i, int j) {
-		// TODO Auto-generated constructor stub
+    
+    
+    
+	public Card(Colour colour, int number, boolean covered) {
+		super();
+		this.colour = colour;
+		this.number = number;
+		this.covered = covered;
 	}
 
+	public Colour getColour() {
+		return colour;
+	}
+
+	public void setColour(Colour colour) {
+		this.colour = colour;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	
 	public boolean isUncovered() {
 		// TODO Auto-generated method stub
 		return false;
