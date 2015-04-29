8b6b93922daea35fc91d70a49863a0d93436bbfd
bl0819 <bl0819@192.168.7.1>
2015-04-29 20:29:37 +0200

    MoveFromDeckToWasteControllerTest algoritmo

--- src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
@@ -1,25 +1,30 @@
 package es.klodike.controllers.test;
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import es.klodike.controllers.MoveFromDeckToWasteController;
-import es.klondike.model.Card;
-import es.klondike.model.Deck;
-import es.klondike.model.Waste;
+import es.klondike.model.Tableau;
+
 
 
 public class MoveFromDeckToWasteControllerTest {
    private MoveFromDeckToWasteController moveFromDeckToWasteController;
+   private Tableau tableau;
+   
 	 @Before
 	 public void before(){
 		 moveFromDeckToWasteController= new MoveFromDeckToWasteController();
+		 tableau = new Tableau();
 	 }
-	//@Test
+	@Test
 	public void moveFromDeckToWasteTest() {
+	
+		  // si hay cartas suficientes
+			// comprobara que deck tiene menos 1 cartas
+		    // comprobar que waste tiene mas 1 cartas. (y q cambió  de cubiertas=true a cubiertas=false
+		  // else
+		     //comprobar que deck tiene el mismo nuemro de cartas
+		     //comprobar que waste tiene el mismo numero de cartas
+	
 	
 	}
 
