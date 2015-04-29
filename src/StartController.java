6a8a6bd9b4136de3c287a79f594386ad29cd8f8e
Usuario <Usuario@aula2-PC2>
2015-04-29 10:47:41 +0200

    UPDATE StartController. getSizeFullDeck()-> ADD nuevo atributo. fullDeckNotRepartido...para saber cartas no repartidas y conservar la baraja completa intacta

--- src/StartController.java
+++ src/StartController.java
@@ -10,6 +10,7 @@ public class StartController {
 	private Stack<Card> deck;
 	private Stack<Card> waste;
 	private List<Card> fullDeck;
+	private List<Card> fullDeckNotRepartida;
 	
 	private void init(){
 		this.initFullDeck();
@@ -29,9 +30,9 @@ public class StartController {
 	private Card getRandomCard() {
 		Random r = new Random();
 		int ordinalFulDeck=0;
-		if(fullDeck.size()>1) r.nextInt(fullDeck.size()-1);
-		Card card= fullDeck.get(ordinalFulDeck);
-		fullDeck.remove(ordinalFulDeck);
+		if(fullDeckNotRepartida.size()>1) r.nextInt(fullDeckNotRepartida.size()-1);
+		Card card= fullDeckNotRepartida.get(ordinalFulDeck);
+		fullDeckNotRepartida.remove(ordinalFulDeck);
 		return card;
 	}
 
@@ -43,22 +44,15 @@ public class StartController {
 	}
 	public void initFullDeck(){
 		Colour[] colours= Colour.values();
-		
-		
-		//generar las 52 cartas...
-		 //hacer una pila de 52..
-		    // con un ciclo... del 1 al 13
+	
 		for(int i=0;i<13;i++){
 			for(Colour colour:colours){
 				Card card = new Card(colour,i+1,true);
 				fullDeck.add(card);
+				fullDeckNotRepartida.add(card);
 			}
 		}		
-	     //revolverlas..
-		    // recorrer la pila y con una pila auxiliar
-		       // irlas poniendo en desorden.. (nuemero aleatoria entre 1 
-		//nota en los metodos init... tomar elementos de esta pila.
-		
+	 
 	}
 	
 	public void initAllFourColours(){
@@ -83,6 +77,7 @@ public class StartController {
 		this.deck= new Stack<Card>();
 		this.waste= new Stack<Card>();
 		this.fullDeck= new ArrayList<Card>();
+		this.fullDeckNotRepartida= new ArrayList<Card>();
 		this.init();
 	}
 
@@ -131,10 +126,18 @@ public class StartController {
 	}
 
 	public Integer getSizeFullDeck() {
-		return 52;
+//		ArrayList<Integer> sizeFourColours = this.getSizeFourColurs();
+//		int numCards = 0;
+//		for (Integer integer : sizeFourColours) {
+//			numCards+=integer;
+//		}
+//		numCards+=this.sizeDeck();
+		return fullDeck.size();
 	}
 
 	public boolean verifyNotRepetedCards() {
+		boolean repetidas= false;
+	
 		return true;
 	}
 
