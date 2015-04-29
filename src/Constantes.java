35804fcafe25939517b292702781054ede4770bd
Usuario <Usuario@aula2-PC2>
2015-04-29 12:21:50 +0200

    ADD Interface Constantes-> ya qeu se necesitan en más de una clase

---
+++ src/Constantes.java
@@ -0,0 +1,7 @@
+
+public interface Constantes {
+	public static final int NUMBER_ALL_FOURCOLOURS = 7;
+	public static final int NUMBER_FOUNDATIONS = 4;
+	public static final int NUMBER_COLOUR_CARD = 13;
+	public static final int NUMBER_CARDS_DECK_INITIAL = 24;
+}
