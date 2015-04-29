d01204b6d3be27c876e619b0936cb5212c30f3a9
bl0819 <bl0819@192.168.7.1>
2015-04-29 20:53:28 +0200

    UPDATE moveFromDectToWasteControllerTest--> incompleto aun

--- src/es/klodike/controllers/MoveFromDeckToWasteController.java
+++ src/es/klodike/controllers/MoveFromDeckToWasteController.java
@@ -2,4 +2,9 @@ package es.klodike.controllers;
 
 public class MoveFromDeckToWasteController {
 
+	public boolean moveFromDeckToWaste() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
 }
--- src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
@@ -1,7 +1,13 @@
 package es.klodike.controllers.test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import es.klodike.controllers.MoveFromDeckToWasteController;
+import es.klondike.model.Card;
 import es.klondike.model.Tableau;
 
 
@@ -17,13 +23,23 @@ public class MoveFromDeckToWasteControllerTest {
 	 }
 	@Test
 	public void moveFromDeckToWasteTest() {
-	
+	      // si está vacia.... el deck tendrá todo el waste entero pero volteadas
+		   int sizeDeck =tableau.getDeck().size();
+		   int sizeWaste =tableau.getWaste().size();
+		   Card card = tableau.getDeck().peek();
+		   if(sizeDeck>= 1){
 		  // si hay cartas suficientes
+			    moveFromDeckToWasteController.moveFromDeckToWaste();
+			    assertEquals(sizeDeck-1, tableau.getDeck().size());
+			    assertEquals(sizeWaste+1, tableau.getWaste().size());
+			    assertFalse(card.isCovered()==tableau.getWaste().peek().isCovered());
 			// comprobara que deck tiene menos 1 cartas
 		    // comprobar que waste tiene mas 1 cartas. (y q cambió  de cubiertas=true a cubiertas=false
-		  // else
+		   }
+		   else{
 		     //comprobar que deck tiene el mismo nuemro de cartas
 		     //comprobar que waste tiene el mismo numero de cartas
+		   }
 	
 	
 	}
