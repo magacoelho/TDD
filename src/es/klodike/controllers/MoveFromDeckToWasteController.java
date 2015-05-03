9998a966591cf0be0d99421823984f7311f8f3a3
magacoelho <mgacoelho@yahoo.es>
2015-05-03 20:23:31 +0200

    UPDATE MoveFromDeckToWaste- Test ok.

--- src/es/klodike/controllers/MoveFromDeckToWasteController.java
+++ src/es/klodike/controllers/MoveFromDeckToWasteController.java
@@ -1,5 +1,6 @@
 package es.klodike.controllers;
 
+import es.klondike.model.Card;
 import es.klondike.model.Tableau;
 
 public class MoveFromDeckToWasteController {
@@ -11,10 +12,18 @@ public class MoveFromDeckToWasteController {
 	}
 
 	public void moveFromDeckToWaste() {
-		
-		tableau.getWaste().push(tableau.getDeck().pop());
-		tableau.getWaste().peek().setCovered(false);
-		
+		if(tableau.getDeck().size()>=1){
+		  tableau.getWaste().push(tableau.getDeck().pop());
+		  tableau.getWaste().peek().setCovered(false);
+		}
+		else{
+		   for(Card card: tableau.getWaste()){
+			   card.setCovered(true);
+		       tableau.getDeck().push(card);
+		      
+	    }
+		   tableau.getWaste().removeAllElements();
+	   }
 	}
 
 }
--- src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromDeckToWasteControllerTest.java
@@ -25,31 +25,35 @@ public class MoveFromDeckToWasteControllerTest {
 	 }
 	@Test
 	public void moveFromDeckToWasteTest() {
-	       for(int i=0; i<Constantes.NUMBER_CARDS_DECK_INITIAL +1;i++){
-		   int sizeDeck =tableau.getDeck().size();
-		   int sizeWaste =tableau.getWaste().size();
-		   Card card = tableau.getDeck().peek();
-		  
-		   moveFromDeckToWasteController.moveFromDeckToWaste();
-		   if(sizeDeck>= 1){
-		  // si hay cartas suficientes
+		
+	      for(int i=0;i<Constantes.NUMBER_CARDS_DECK_INITIAL;i++){
+	    	  int sizeDeck =tableau.getDeck().size();
+		      int sizeWaste =tableau.getWaste().size();
+	
+		      moveFromDeckToWasteController.moveFromDeckToWaste();
+		     if(sizeDeck>= 1){
+		     	// si hay cartas suficientes
 			    assertEquals(sizeDeck-1, tableau.getDeck().size());
 			    assertEquals(sizeWaste+1, tableau.getWaste().size());
 			    assertFalse(tableau.getWaste().peek().isCovered());
 			// comprobara que deck tiene menos 1 cartas
 		    // comprobar que waste tiene mas 1 cartas. (y q cambió  de cubiertas=true a cubiertas=false
 		   }
-		   else{
-			    // verificar que en el Deck están las cartas del Waste
-			   assertEquals(sizeWaste, tableau.getDeck().size());
-			   assertEquals(0, tableau.getWaste().size());
-			   for(int j=0;j<tableau.getDeck().size();j++)
-			       assertTrue(tableau.getDeck().pop().isCovered());
+          }
+	 
+	      // verificar que en el Deck están las cartas del Waste
+	      int sizeDeck =tableau.getDeck().size();
+	      int sizeWaste =tableau.getWaste().size();
+	       moveFromDeckToWasteController.moveFromDeckToWaste();
+	       assertEquals(sizeWaste, tableau.getDeck().size());
+		   assertEquals(0, tableau.getWaste().size());
+		   for(int j=0;j<tableau.getDeck().size();j++)
+		       assertTrue(tableau.getDeck().peek().isCovered());
 		     //comprobar que deck tiene el mismo nuemro de cartas
 		     //comprobar que waste tiene el mismo numero de cartas
-		   }
-	
+		  
 	      }
-	}
+	      
+
 
 }
