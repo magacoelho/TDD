5d49eb9cd1063539f73d6a335e71fb1a6349f1da
magacoelho <mgacoelho@yahoo.es>
2015-05-03 21:19:43 +0200

    INITIAL MoveFromWasteToFoundationTest

---
+++ src/es/klodike/controllers/test/MoveFromWasteToFoundationControllerTest.java
@@ -0,0 +1,14 @@
+package es.klodike.controllers.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveFromWasteToFoundationControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
