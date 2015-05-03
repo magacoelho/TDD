44d079677a8519d77a27e91c8c5bd7253bb52a29
magacoelho <mgacoelho@yahoo.es>
2015-05-03 21:23:15 +0200

    ADD MoveFromWasteToFoundationController class empty UPDATE Contructror MoveFromWasteToFoundationControllerTest pasanod el Tableau

---
+++ src/es/klodike/controllers/MoveFromWasteToFoundationController.java
@@ -0,0 +1,11 @@
+package es.klodike.controllers;
+
+import es.klondike.model.Tableau;
+
+public class MoveFromWasteToFoundationController {
+
+	public MoveFromWasteToFoundationController(Tableau tableau) {
+		
+	}
+ 
+}
--- src/es/klodike/controllers/test/MoveFromWasteToFoundationControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromWasteToFoundationControllerTest.java
@@ -2,13 +2,23 @@ package es.klodike.controllers.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import es.klodike.controllers.MoveFromDeckToWasteController;
+import es.klodike.controllers.MoveFromWasteToFoundationController;
+import es.klondike.model.Tableau;
 
 public class MoveFromWasteToFoundationControllerTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	 private Tableau tableau;
+	private MoveFromWasteToFoundationController moveFromWasteToFoundationController;
+
+	@Before
+	 public void before(){
+		 this.tableau = new Tableau();
+		 this.tableau.init();
+		 this.moveFromWasteToFoundationController= new  MoveFromWasteToFoundationController(this.tableau);
+	 }
 
 }
