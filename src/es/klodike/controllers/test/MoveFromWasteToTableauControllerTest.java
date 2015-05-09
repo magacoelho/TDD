093f0aa741d8a294165139b23ee65ee1dd730806
magacoelho <mgacoelho@yahoo.es>
2015-05-09 19:52:46 +0200

    Rename clases

---
+++ src/es/klodike/controllers/test/MoveFromWasteToTableauControllerTest.java
@@ -0,0 +1,24 @@
+package es.klodike.controllers.test;
+
+import org.junit.Before;
+
+import es.klodike.controllers.KlondikeController;
+import es.klodike.controllers.MoveFromWasteToTableauController;
+import es.klondike.model.Board;
+
+
+
+public class MoveFromWasteToTableauControllerTest {
+	private MoveFromWasteToTableauController moveFormWasteToTableauController;
+
+
+	@Before
+	public void before(){
+		 moveFormWasteToTableauController = new MoveFromWasteToTableauController(new Board()); 
+		 this.board = moveFormWasteToTableauController.getBoard();
+		 
+		 
+		
+	}
+
+}
