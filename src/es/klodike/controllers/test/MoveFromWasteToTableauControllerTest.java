18ac0b923cae0d82e64f2359d0cee039a817e27e
magacoelho <mgacoelho@yahoo.es>
2015-05-09 20:00:37 +0200

    caso 1: Waste no vacio y tableauTarget Vacio

--- src/es/klodike/controllers/test/MoveFromWasteToTableauControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromWasteToTableauControllerTest.java
@@ -1,6 +1,9 @@
 package es.klodike.controllers.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import es.klodike.controllers.KlondikeController;
 import es.klodike.controllers.MoveFromWasteToTableauController;
@@ -17,8 +20,12 @@ public class MoveFromWasteToTableauControllerTest {
 	public void before(){
 		 moveFormWasteToTableauController = new MoveFromWasteToTableauController(new Board()); 
 		 this.board = moveFormWasteToTableauController.getBoard();
-		 
-		 
+		 this.board.init();
+	}
+	
+	@Test 
+	public void moveFromWasteToEmptyTableau(){
+		assertFalse(moveFormWasteToTableauController.isEmptyWaste());
 		
 	}
 
