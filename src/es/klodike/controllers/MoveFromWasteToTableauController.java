43ecb266673c33dc97de034ed465bd631c2b284c
magacoelho <mgacoelho@yahoo.es>
2015-05-09 20:40:51 +0200

    Error implementando metodo move con funcionalidades minimas

--- src/es/klodike/controllers/MoveFromWasteToTableauController.java
+++ src/es/klodike/controllers/MoveFromWasteToTableauController.java
@@ -1,11 +1,27 @@
 package es.klodike.controllers;
 
+import java.util.Stack;
+
 import es.klondike.model.Board;
+import es.klondike.model.Card;
 
 public class MoveFromWasteToTableauController  extends KlondikeController{
 
 	public MoveFromWasteToTableauController(Board board) {
 		super(board);
 	}
+
+	public void move(int tableauTargetIndex) {
+		Card cardTopWaste =this.getBoard().getWaste().peek();
+		Stack<Card> tableauTarget =   this.getBoard().getTableaus().get(tableauTargetIndex);
+	    tableauTarget.push(cardTopWaste);
+	    this.getBoard().getWaste().pop();
+		
+	}
+
+	
+	
+
+
 
 }
--- src/es/klodike/controllers/test/MoveFromWasteToTableauControllerTest.java
+++ src/es/klodike/controllers/test/MoveFromWasteToTableauControllerTest.java
@@ -5,28 +5,52 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import es.klodike.controllers.KlondikeController;
+
 import es.klodike.controllers.MoveFromWasteToTableauController;
 import es.klondike.model.Board;
+import es.klondike.model.Card;
+import es.klondike.model.Suite;
+import es.klondike.utils.Constantes;
 
 
 
 public class MoveFromWasteToTableauControllerTest {
-	private MoveFromWasteToTableauController moveFormWasteToTableauController;
+	private MoveFromWasteToTableauController moveFromWasteToTableauController;
 	private Board board;
 
 
 	@Before
 	public void before(){
-		 moveFormWasteToTableauController = new MoveFromWasteToTableauController(new Board()); 
-		 this.board = moveFormWasteToTableauController.getBoard();
+		 moveFromWasteToTableauController = new MoveFromWasteToTableauController(new Board()); 
+		 this.board = moveFromWasteToTableauController.getBoard();
 		 this.board.init();
 	}
 	
 	@Test 
 	public void moveFromWasteToEmptyTableau(){
-		assertFalse(moveFormWasteToTableauController.isEmptyWaste());
+	  // precondiciones waste no vacio y cartadeWaste = 13
 		
-	}
-
+	 // tableau destino_> vacia 
+		int tableauTarget=0;
+		Card cardToMove =new Card(Suite.CLUB, 13, Constantes.UNCOVERED_CARD);
+		board.getWaste().push(cardToMove);
+		assertFalse(moveFromWasteToTableauController.isEmptyWaste());
+		assertEquals(13, board.getWaste().peek().getNumber());
+		assertTrue(board.getFoundations().get(tableauTarget).size()==0);
+       	moveFromWasteToTableauController.move(tableauTarget);
+		assertEquals(cardToMove, this.board.getTableaus().get(tableauTarget).peek());
+		assertEquals(1,this.board.getTableaus().get(tableauTarget).size());
+		
+		tableauTarget=0;
+		cardToMove =new Card(Suite.CLUB, 12, Constantes.UNCOVERED_CARD);
+		board.getWaste().push(cardToMove);
+		assertFalse(moveFromWasteToTableauController.isEmptyWaste());
+		assertNotEquals(13, board.getWaste().peek().getNumber());
+		assertTrue(board.getFoundations().get(tableauTarget).size()==0);
+       	moveFromWasteToTableauController.move(tableauTarget);
+		assertNull(this.board.getTableaus().get(tableauTarget).peek());
+		assertEquals(0,this.board.getTableaus().get(tableauTarget).size());
+		}
+	
+        
 }
